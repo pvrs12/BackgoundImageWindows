97a9f89f085296faf60048dc31637797e910369f
pvrs12 <pvrs12@gmail.com>
2019-03-27 15:19:29 -0400

    Added selection dialog for 8 available wallpapers

--- BackgroundImage/BackgroundImage/Program.cs
+++ BackgroundImage/BackgroundImage/Program.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 
 namespace BackgroundImage
 {
@@ -12,43 +15,120 @@ namespace BackgroundImage
     {
         const string BASE = "http://www.bing.com";
 
-        static string getImageURL()
+        private static string selected_url = "";
+        private static List<PictureBox> images;
+
+        private static WallpaperSelector ws;
+
+        static List<string> getImageURLs(int number)
         {
             HttpClient client = new HttpClient();
-            string resource = "/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US";
-            Task<HttpResponseMessage> task = client.GetAsync(BASE + resource);
-            task.Wait();
-            HttpResponseMessage resp = task.Result;
-            Task<string> stringtask = resp.Content.ReadAsStringAsync();
-            stringtask.Wait();
-            string result = stringtask.Result;
+            string resource = String.Format("/HPImageArchive.aspx?format=js&n={0}&mkt=en-US", number);
+            Task<HttpResponseMessage> t1 = client.GetAsync(BASE + resource);
+            t1.Wait();
+            Task<string> t2= t1.Result.Content.ReadAsStringAsync();
+            t2.Wait();
 
-            JObject resultJson = JObject.Parse(result);
-            string imageResource = resultJson["images"][0]["url"].ToString();
+            JObject resultJson = JObject.Parse(t2.Result);
 
-            return imageResource;
+            List<string> images = new List<string>();
+            foreach(JObject image in resultJson["images"])
+            {
+                images.Add(String.Format("{0}{1}", BASE, image["url"].ToString()));
+            }
+
+            return images;
         }
 
         static void setWallpaper(string url)
         {
-            Uri uri = new Uri(BASE + url);
+            Uri uri = new Uri(url);
             Wallpaper.Set(uri, Wallpaper.Style.Stretched);
+        }
+
+        private static WallpaperSelector run()
+        {
+            WallpaperSelector ws = new WallpaperSelector();
+
+            ws.previewButton.Click += PreviewButton_Click;
+            ws.setWallpaperButton.Click += SetWallpaperButton_Click;
+
+            List<string> imageURLs = getImageURLs(8);
+
+            // download the images
+            
+            List<Tuple<Task<HttpResponseMessage>, string>> httpTasks = new List<Tuple<Task<HttpResponseMessage>, string>>();
+            foreach (string imageURL in imageURLs)
+            {
+                HttpClient client = new HttpClient();
+                httpTasks.Add(Tuple.Create(client.GetAsync(imageURL), imageURL));
+            }
+
+            // load the streams
+            List<Tuple <Task<Stream>, string>> streamTasks = new List<Tuple<Task<Stream>, string>>();
+            foreach(Tuple < Task<HttpResponseMessage>, string> t in httpTasks)
+            {
+                t.Item1.Wait();
+                streamTasks.Add(Tuple.Create(t.Item1.Result.Content.ReadAsStreamAsync(), t.Item2));
+            }
+
+            // read from the streams
+            images = new List<PictureBox>();
+            foreach(Tuple<Task<Stream>, string> stream in streamTasks)
+            {
+                PictureBox pb = new PictureBox();
+                stream.Item1.Wait();
+                pb.Width = 250;
+                pb.Height = (int)(pb.Width * (1080.0 / 1920.0));
+
+                pb.Image = Image.FromStream(stream.Item1.Result).GetThumbnailImage(pb.Width, pb.Height, null, IntPtr.Zero);
+                pb.Parent = ws.flowLayoutPanel1;
+
+                pb.Name = stream.Item2;
+
+                pb.Click += Pb_Click;
+                images.Add(pb);
+            }
+            
+            return ws;
+        }
+
+        private static void SetWallpaperButton_Click(object sender, EventArgs e)
+        {
+            setWallpaper(selected_url);
+        }
+
+        private static void PreviewButton_Click(object sender, EventArgs e)
+        {
+            //open the image in a web browser
+            System.Diagnostics.Process.Start(selected_url);
+        }
+
+        private static void clear_selections()
+        {
+            foreach(PictureBox pb in images)
+            {
+                pb.BorderStyle = BorderStyle.None;
+            }
+        }
+
+        private static void Pb_Click(object sender, EventArgs e)
+        {
+            ws.previewButton.Enabled = true;
+            ws.setWallpaperButton.Enabled = true;
+
+            clear_selections();
+
+            PictureBox pb = (PictureBox)sender;
+            pb.BorderStyle = BorderStyle.Fixed3D;
+
+            selected_url = pb.Name;
         }
 
         static void Main(string[] args)
         {
-            string imageURL = getImageURL();
-            System.Diagnostics.Process.Start(BASE + imageURL);
-            System.Threading.Thread.Sleep(2000);
-            DialogResult result = MessageBox.Show(
-                "Do you want to change the background?",
-                "Background Downloader",
-                MessageBoxButtons.YesNo);
-            if (result == DialogResult.Yes)
-            {
-                setWallpaper(imageURL);
-            }
-
+            ws = run();
+            ws.ShowDialog();
         }
     }
 }
---
+++ BackgroundImage/BackgroundImage/WallpaperSelector.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace BackgroundImage
+{
+    partial class WallpaperSelector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.previewButton = new System.Windows.Forms.Button();
+            this.setWallpaperButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel2);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1038, 342);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.previewButton);
+            this.flowLayoutPanel2.Controls.Add(this.setWallpaperButton);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(1041, 32);
+            this.flowLayoutPanel2.TabIndex = 0;
+            // 
+            // previewButton
+            // 
+            this.previewButton.Enabled = false;
+            this.previewButton.Location = new System.Drawing.Point(3, 3);
+            this.previewButton.Name = "previewButton";
+            this.previewButton.Size = new System.Drawing.Size(75, 23);
+            this.previewButton.TabIndex = 2;
+            this.previewButton.Text = "Preview";
+            this.previewButton.UseVisualStyleBackColor = true;
+            // 
+            // setWallpaperButton
+            // 
+            this.setWallpaperButton.Enabled = false;
+            this.setWallpaperButton.Location = new System.Drawing.Point(84, 3);
+            this.setWallpaperButton.Name = "setWallpaperButton";
+            this.setWallpaperButton.Size = new System.Drawing.Size(110, 23);
+            this.setWallpaperButton.TabIndex = 3;
+            this.setWallpaperButton.Text = "Set Wallpaper";
+            this.setWallpaperButton.UseVisualStyleBackColor = true;
+            // 
+            // WallpaperSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1038, 342);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "WallpaperSelector";
+            this.Text = "WallpaperSelecter";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        public System.Windows.Forms.Button previewButton;
+        public System.Windows.Forms.Button setWallpaperButton;
+    }
+}
